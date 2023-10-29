95a5816c161c3e7dfd389c08782b4ace86e3a282
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2023-10-29 18:38:40 +0530

    Update script.js

--- speedtest/script.js
+++ speedtest/script.js
@@ -1,34 +1,19 @@
 const startButton = document.getElementById('start-button');
 const progress = document.getElementById('progress');
-const speedText = document.querySelector('.speed-text');
+const speedRange = document.getElementById('speedRange');
 
 startButton.addEventListener('click', () => {
     startButton.disabled = true;
     let count = 0;
     const interval = setInterval(() => {
-        count += 10;
-        progress.style.transform = `rotate(${count}deg)`;
+        count += 1;
+        const speed = count + " Mbps";
+        progress.style.transform = `rotate(${(count / 100) * 180}deg)`;
+        speedRange.textContent = speed;
 
-        if (count >= 180) {
+        if (count >= 100) {
             clearInterval(interval);
             startButton.disabled = false;
-            simulateSpeed(); // Simulating speed change
         }
-    }, 100);
+    }, 50);
 });
-
-// Function to simulate speed change
-function simulateSpeed() {
-    let speed = 0;
-    const speedIncrement = 50; // Change in speed
-
-    // Simulate increase in speed (just for demonstration)
-    const speedInterval = setInterval(() => {
-        speed += speedIncrement;
-        if (speed >= 100) {
-            speed = 100;
-            clearInterval(speedInterval);
-        }
-        speedText.textContent = speed;
-    }, 500); // Change this interval for a more realistic representation (simulated value)
-}
