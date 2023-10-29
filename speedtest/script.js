c7797ff45f992793d75b7eba21d61db4b7e335c7
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2023-10-29 18:47:35 +0530

    Update script.js

--- speedtest/script.js
+++ speedtest/script.js
@@ -1,19 +1,25 @@
 const startButton = document.getElementById('start-button');
-const progress = document.getElementById('progress');
-const speedRange = document.getElementById('speedRange');
+const downloadSpeedElement = document.getElementById('download-speed');
+const uploadSpeedElement = document.getElementById('upload-speed');
 
 startButton.addEventListener('click', () => {
     startButton.disabled = true;
-    let count = 0;
-    const interval = setInterval(() => {
-        count += 1;
-        const speed = count + " Mbps";
-        progress.style.transform = `rotate(${(count / 100) * 180}deg)`;
-        speedRange.textContent = speed;
 
-        if (count >= 100) {
-            clearInterval(interval);
-            startButton.disabled = false;
-        }
-    }, 50);
+    // Simulated file sizes in MB
+    const downloadSize = 10; // MB
+    const uploadSize = 5; // MB
+
+    const downloadSpeed = calculateSpeed(downloadSize);
+    const uploadSpeed = calculateSpeed(uploadSize);
+
+    downloadSpeedElement.textContent = downloadSpeed.toFixed(2);
+    uploadSpeedElement.textContent = uploadSpeed.toFixed(2);
+
+    startButton.disabled = false;
 });
+
+function calculateSpeed(fileSize) {
+    const startTime = new Date().getTime();
+    const downloadSpeed = (fileSize * 8) / ((new Date().getTime() - startTime) / 1000); // Mbps
+    return downloadSpeed;
+}
