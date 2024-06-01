f99a83b5b8b48e1b0ece47a993af901b1c26ce61
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2024-06-01 16:07:52 +0530

    Delete variables.js

--- variables.js
+++
@@ -1,7 +0,0 @@
-// Define your variables here
-var welcomeMessage = "KAMARJAHAN SEARCH ENGINE";
-var googleSearchURL = "https://www.google.com/search?q=";
-var pageTitle = "KAMARJAHAN SEARCH ENGINE";
-var headingText = "KAMARJAHAN SEARCH ENGINE";
-
-
