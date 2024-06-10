d0334b5b5e897c55026cd4aecb48d2b2d6b8ed80
KAMARJAHAN <ztenkammu@gmail.com>
2024-06-10 19:47:57 +0530

    Update hcks.js

--- hcks.js
+++ hcks.js
@@ -1,16 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('hack-form');
-    const loadingSection = document.getElementById('loading');
-    const hackSection = document.getElementById('hack-section');
-    const usernameInput = document.getElementById('username');
+document.getElementById('hack-btn').addEventListener('click', function() {
+    var username = document.getElementById('username').value;
+    if (username) {
+        document.getElementById('hacking-form').classList.add('hidden');
+        document.getElementById('loading-screen').classList.remove('hidden');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        hackSection.style.display = 'none';
-        loadingSection.style.display = 'flex';
-
-        setTimeout(() => {
-            window.location.href = 'anhck.html?username=' + encodeURIComponent(usernameInput.value);
-        }, 10000); // 10 seconds
-    });
+        setTimeout(function() {
+            window.location.href = 'anhck.html';
+        }, 10000);
+    } else {
+        alert('Please enter a username.');
+    }
 });
