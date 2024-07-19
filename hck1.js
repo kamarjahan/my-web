a92b85ffc964d70e266a552b4c35c1c168c486b3
KAMARJAHAN <ztenkammu@gmail.com>
2024-07-19 10:51:54 +0530

    Create hck1.js

---
+++ hck1.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('hack-form');
+    const loadingSection = document.getElementById('loading');
+    const hackSection = document.getElementById('hack-section');
+    const usernameInput = document.getElementById('username');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        hackSection.style.display = 'none';
+        loadingSection.style.display = 'flex';
+
+        setTimeout(() => {
+            window.location.href = 'anhck.html?username=';
+        }, 10000); // 10 seconds
+    });
+});
